76b698742c4ac0d2982325e891eec7169a56f370
Harshit Vijay <harshitvj07@gmail.com>
2024-06-14 02:01:13 +0530

    Added tests for all hardware, os, network, basic & all

--- tests/test.rs
+++ tests/test.rs
@@ -2,20 +2,49 @@
 mod tests {
     use std::process::Command;
 
+    
     #[test]
     fn test_ospect_runs() {
         let result = Command::new("target/release/ospect") // Specify the binary path
             .output()
-            .expect("Failed to execute command");
+            .expect("Failed to execute command (BASIC)");
         assert!(result.status.success());
     }
+    
+    #[test]
+    fn test_ospect_network_runs() {
+        let result = Command::new("target/release/ospect") // Specify the binary path
+            .arg("network")
+            .output()
+            .expect("Failed to execute command (NETWORK)");
+        assert!(result.status.success());
+    }
+
+    #[test]
+    fn test_ospect_os_runs() {
+        let result = Command::new("target/release/ospect") // Specify the binary path
+            .arg("os")
+            .output()
+            .expect("Failed to execute command (OS)");
+        assert!(result.status.success());
+    }
+
+    #[test]
+    fn test_ospect_hardware_runs() {
+        let result = Command::new("target/release/ospect") // Specify the binary path
+            .arg("hardware")
+            .output()
+            .expect("Failed to execute command (HARDWARE)");
+        assert!(result.status.success());
+    }
+
 
     #[test]
     fn test_ospect_all_runs() {
         let result = Command::new("target/release/ospect") // Specify the binary path
             .arg("all")
             .output()
-            .expect("Failed to execute command");
+            .expect("Failed to execute command (ALL)");
         assert!(result.status.success());
     }
 }
