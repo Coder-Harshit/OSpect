bc517090c30f6b8d62840b137f771c5b7777e843
Harshit Vijay <harshitvj07@gmail.com>
2024-09-09 17:38:05 +0530

    test file changes

--- tests/test.rs
+++ tests/test.rs
@@ -1,16 +1,29 @@
 #[cfg(test)]
-mod tests {
-    use std::process::Command;
+    mod tests {
+        use std::process::Command;
+        use std::env;
 
+        #[test]
+        fn test_ospect_runs() {
+            let binary_path = env::var("OSPECT_BINARY_PATH").unwrap_or("target/release/ospect".to_string()); // Adjust if needed
+            let result = Command::new(binary_path)
+                .output()
+                .expect("Failed to execute command");
 
-    #[test]
-    fn test_ospect_runs() {
-        let result = Command::new("target/release/ospect") // Specify the binary path
-            .output()
-            .expect("Failed to execute command (BASIC)");
-        assert!(result.status.success(), "Command failed with exit code: {}", result.status.code().unwrap_or(-1));
+            assert!(result.status.success(), "Command failed with exit code: {}", result.status.code().unwrap_or(-1));
+
+            // Optional: Check output or stderr
+            if let Ok(output) = String::from_utf8(result.stdout) {
+                // Assert on output content
+                assert!(output.contains("Basic System Information"));
+            }
+
+            if let Ok(stderr) = String::from_utf8(result.stderr) {
+                // Check for error messages
+                assert!(!stderr.contains("error"));
+            }
+        }
     }
-
 //     #[test]
 //     fn test_ospect_network_runs() {
 //         let result = Command::new("target/release/ospect") // Specify the binary path
@@ -47,4 +60,3 @@ mod tests {
 //             .expect("Failed to execute command (ALL)");
 //         assert!(result.status.success());
 //     }
-}
